eae75e377f843c3d3885e3b0d6f9ea80cf76ed12
emanu <emanu@DESKTOP-SSGID88>
2022-12-15 15:40:46 +0100

    Setup

--- src/Script/sql/setup.sql
+++ src/Script/sql/setup.sql
@@ -1,10 +1,31 @@
-DROP TABLE IF EXISTS CONTACT;
+DROP TABLE IF EXISTS CATEGORIE;
 
-create table CONTACT(
-	contact_id serial primary key,
-	first_name varchar(20) not null,
-	last_name varchar(20) not null,
-	phone varchar(20) not null unique
+create table CATEGORIE(
+	categorie_id serial primary key,
+	category_name varchar(20) not null
+	
 );
 
-insert into contact(first_name, last_name, phone) values('Mario', 'Rossi', '+3928361836481');
+insert into CATEGORIE(category_name) values('Pizze');
+insert into CATEGORIE(category_name) values('Pinse');
+insert into CATEGORIE(category_name) values('Dolci');
+insert into CATEGORIE(category_name) values('Bevande');
+insert into CATEGORIE(category_name) values('Antipasto');
+
+
+DROP TABLE IF EXISTS MENU;
+create table MENU(
+	menu_id serial primary key,
+	menu_name varchar(20) not null,
+	price float not null,
+	description varchar(60),
+	categorie_id int,	
+	foreign key (categorie_id) references CATEGORIE (categorie_id)  on delete cascade
+	
+	
+	
+);
+
+DROP TABLE IF EXISTS ordini;
+create table ordini(
+
